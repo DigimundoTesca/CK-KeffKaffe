114c951590f47934e4767bac9f153ccf1889d8fa
Ramsés Martinez <ramses.mtz96@gmail.com>
2017-04-03 21:36:09 -0500

    changed names

--- cloudkitchen/settings/dev_postgres.py
+++ cloudkitchen/settings/dev_postgres.py
@@ -3,10 +3,10 @@ from cloudkitchen.settings.dev import *
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('CLOUDKITCHEN_DB_NAME'),
-        'USER': os.getenv('CLOUDKITCHEN_DB_USER'),
-        'PASSWORD': os.getenv('CLOUDKITCHEN_DB_PASSWORD'),
-        'HOST': os.getenv('CLOUDKITCHEN_DB_HOST'),
-        'PORT': os.getenv('CLOUDKITCHEN_DB_PORT'),
+        'NAME': os.getenv('KEFFKAFFE_DB_NAME'),
+        'USER': os.getenv('KEFFKAFFE_DB_USER'),
+        'PASSWORD': os.getenv('KEFFKAFFE_DB_PASSWORD'),
+        'HOST': os.getenv('KEFFKAFFE_DB_HOST'),
+        'PORT': os.getenv('KEFFKAFFE_DB_PORT'),
     }
 }
--- cloudkitchen/settings/prod.py
+++ cloudkitchen/settings/prod.py
@@ -9,11 +9,11 @@ SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.getenv('CLOUDKITCHEN_DB_NAME'),
-        'USER': os.getenv('CLOUDKITCHEN_DB_USER'),
-        'PASSWORD': os.getenv('CLOUDKITCHEN_DB_PASSWORD'),
-        'HOST': os.getenv('CLOUDKITCHEN_DB_HOST'),
-        'PORT': os.getenv('CLOUDKITCHEN_DB_PORT'),
+        'NAME': os.getenv('KEFFKAFFE_DB_NAME'),
+        'USER': os.getenv('KEFFKAFFE_DB_USER'),
+        'PASSWORD': os.getenv('KEFFKAFFE_DB_PASSWORD'),
+        'HOST': os.getenv('KEFFKAFFE_DB_HOST'),
+        'PORT': os.getenv('KEFFKAFFE_DB_PORT'),
     }
 }
 
